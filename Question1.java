e0df476a1783e74f95222ac664bee0129ad46ada
hs98012 <125025316+hs98012@users.noreply.github.com>
2023-07-05 14:50:14 +0900

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int dNum = sc.nextInt();
+        long[] array = new long[dNum + 1];
+        int qNum = sc.nextInt();
+        long[] sArray = new long[dNum + 1];
+        array[0] = 0;
+        sArray[0] = 0;
+        for (int i = 1; i <= dNum ; i++) {
+            array[i] = sc.nextInt();
+            System.out.println("array"+"["+i+"]"+" = " + array[i]);
+            sArray[i] = sArray[i-1] + array[i];
+        }
+        for (int i = 0; i < qNum; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+
+            System.out.println(sArray[end] - sArray[start-1]);
+        }
+    }
+}
