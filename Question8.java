8ed3f8c41f1567cc5b9d0d93b5a510fb601ce246
hs98012 <125025316+hs98012@users.noreply.github.com>
2023-07-24 12:20:52 +0900

    commit

--- Question8.java
+++ Question8.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Question9 {
+public class Question7 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
