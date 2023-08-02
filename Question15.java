ee1330b172b0a2eddae327ef20fcd1f5ada7c039
hs98012 <125025316+hs98012@users.noreply.github.com>
2023-08-02 13:54:04 +0900

    Add files via upload

---
+++ Question15.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Question15 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        Arrays.sort(array);
+        for (int i = 0; i < n; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
